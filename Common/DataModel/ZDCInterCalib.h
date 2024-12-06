82f358888c308afb169463df17dcc0935126f3f4
Chiara Oppedisano <chiara.oppedisano@to.infn.it>
2024-12-06 10:01:43 +0100

    coding convention

--- Common/DataModel/ZDCInterCalib.h
+++ Common/DataModel/ZDCInterCalib.h
@@ -11,7 +11,6 @@
 ///
 /// \brief Data model for ZN tower intercalibration
 /// \author chiara.oppedisano@cern.ch
-
 #ifndef COMMON_DATAMODEL_ZDCINTERCALIB_H_
 #define COMMON_DATAMODEL_ZDCINTERCALIB_H_
 
@@ -21,34 +20,34 @@ namespace o2::aod
 {
 namespace znoutput
 {
-DECLARE_SOA_COLUMN(tdcZNA, ZNAtime, float);     //! ZNA tdc value
-DECLARE_SOA_COLUMN(tdcZNC, ZNCtime, float);     //! ZNC tdc value
-DECLARE_SOA_COLUMN(pmcZNA, ZNAcommonPM, float); //! PMC ZNA
-DECLARE_SOA_COLUMN(pm1ZNA, ZNAPM1, float);      //! PM1 ZNA
-DECLARE_SOA_COLUMN(pm2ZNA, ZNAPM2, float);      //! PM2 ZNA
-DECLARE_SOA_COLUMN(pm3ZNA, ZNAPM3, float);      //! PM3 ZNA
-DECLARE_SOA_COLUMN(pm4ZNA, ZNAPM4, float);      //! PM4 ZNA
-DECLARE_SOA_COLUMN(pmcZNC, ZNCcommonPM, float); //! PMC ZNC
-DECLARE_SOA_COLUMN(pm1ZNC, ZNCPM1, float);      //! PM1 ZNC
-DECLARE_SOA_COLUMN(pm2ZNC, ZNCPM2, float);      //! PM2 ZNC
-DECLARE_SOA_COLUMN(pm3ZNC, ZNCPM3, float);      //! PM3 ZNC
-DECLARE_SOA_COLUMN(pm4ZNC, ZNCPM4, float);      //! PM4 ZNC
+DECLARE_SOA_COLUMN(ZNAtdc, timeZNA, float);     //! ZNA tdc value
+DECLARE_SOA_COLUMN(ZNCtdc, timeZNC, float);     //! ZNC tdc value
+DECLARE_SOA_COLUMN(ZNApmc, znacommonPM, float); //! PMC ZNA
+DECLARE_SOA_COLUMN(ZNApm1, znaPM1, float);      //! PM1 ZNA
+DECLARE_SOA_COLUMN(ZNApm2, znaPM2, float);      //! PM2 ZNA
+DECLARE_SOA_COLUMN(ZNApm3, znaPM3, float);      //! PM3 ZNA
+DECLARE_SOA_COLUMN(ZNApm4, znaPM4, float);      //! PM4 ZNA
+DECLARE_SOA_COLUMN(ZNCpmc, znccommonPM, float); //! PMC ZNC
+DECLARE_SOA_COLUMN(ZNCpm1, ZNCPM1, float);      //! PM1 ZNC
+DECLARE_SOA_COLUMN(ZNCpm2, zncPM2, float);      //! PM2 ZNC
+DECLARE_SOA_COLUMN(ZNCpm3, zncPM3, float);      //! PM3 ZNC
+DECLARE_SOA_COLUMN(ZNCpm4, zncPM4, float);      //! PM4 ZNC
 
 } // namespace znoutput
 
 DECLARE_SOA_TABLE(ZDCInterCalib, "AOD", "ZDCIC", o2::soa::Index<>,
-                  znoutput::tdcZNA,
-                  znoutput::tdcZNC,
-                  znoutput::pmcZNA,
-                  znoutput::pm1ZNA,
-                  znoutput::pm2ZNA,
-                  znoutput::pm3ZNA,
-                  znoutput::pm4ZNA,
-                  znoutput::pmcZNC,
-                  znoutput::pm1ZNC,
-                  znoutput::pm2ZNC,
-                  znoutput::pm3ZNC,
-                  znoutput::pm4ZNC);
+                  znoutput::ZNAtdc,
+                  znoutput::ZNCtdc,
+                  znoutput::ZNApmc,
+                  znoutput::ZNApm1,
+                  znoutput::ZNApm2,
+                  znoutput::ZNApm3,
+                  znoutput::ZNApm4,
+                  znoutput::ZNCpmc,
+                  znoutput::ZNCpm1,
+                  znoutput::ZNCpm2,
+                  znoutput::ZNCpm3,
+                  znoutput::ZNCpm4);
 } // namespace o2::aod
 
 #endif // COMMON_DATAMODEL_ZDCINTERCALIB_H_
